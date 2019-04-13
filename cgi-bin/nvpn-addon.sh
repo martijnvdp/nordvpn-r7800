a28c4f6b16afb487db4119d632558776864b9538
martijnxd <49381565+martijnxd@users.noreply.github.com>
2019-04-13 17:50:29 +0200

    Update nvpn-addon.sh

--- cgi-bin/nvpn-addon.sh
+++ cgi-bin/nvpn-addon.sh
@@ -3,6 +3,7 @@ then
 curl --silent https://nordvpn.com/wp-admin/admin-ajax.php?action=servers_countries
 else
 /etc/init.d/openvpn-client stop
+/etc/init.d/dnscrypt-proxy-2 stop
 if [$1 != "stop"]
 then
 server="$(curl --silent 'https://nordvpn.com/wp-admin/admin-ajax.php?action=servers_recommendations&filters=\{%22country_id%22:'$1'\}' | jq --raw-output 'limit(1;.[])|.hostname')"
@@ -15,5 +16,6 @@ echo push \"rcvbuf 393216\" >> /etc/openvpn/config/client/nordvpn.ovpn
 echo push \"sndbuf 393216\" >> /etc/openvpn/config/client/nordvpn.ovpn
 sed -i 's/mssfix 1450/mssfix 1460/g' /etc/openvpn/config/client/nordvpn.ovpn
 /etc/init.d/openvpn-client start
+/etc/init.d/dnscrypt-proxy-2 start
 fi
 fi
