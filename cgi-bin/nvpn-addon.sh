fc459359ed64b9afd191b8355ac4d1d3a88e6f2f
martijnxd <49381565+martijnxd@users.noreply.github.com>
2019-04-13 17:31:40 +0200

    add stop vpn function

--- cgi-bin/nvpn-addon.sh
+++ cgi-bin/nvpn-addon.sh
@@ -3,6 +3,8 @@ then
 curl --silent https://nordvpn.com/wp-admin/admin-ajax.php?action=servers_countries
 else
 /etc/init.d/openvpn-client stop
+if [$1 != "stop"]
+then
 server="$(curl --silent 'https://nordvpn.com/wp-admin/admin-ajax.php?action=servers_recommendations&filters=\{%22country_id%22:'$1'\}' | jq --raw-output 'limit(1;.[])|.hostname')"
 wget -O /etc/openvpn/config/client/nordvpn.ovpn https://downloads.nordcdn.com/configs/files/ovpn_legacy/servers/$server.udp1194.ovpn
 sed -i 's/auth-user-pass/auth-user-pass nordvpn.auth/g' /etc/openvpn/config/client/nordvpn.ovpn
@@ -13,4 +15,5 @@ echo push \"rcvbuf 393216\" >> /etc/openvpn/config/client/nordvpn.ovpn
 echo push \"sndbuf 393216\" >> /etc/openvpn/config/client/nordvpn.ovpn
 sed -i 's/mssfix 1450/mssfix 1460/g' /etc/openvpn/config/client/nordvpn.ovpn
 /etc/init.d/openvpn-client start
+fi
 fi
